2fcc79ade7fac83d6a5d5ad85e815de0e640466c
Elena Martina <ele_na98@live.it>
2019-06-11 17:29:44 +0100

    API request working

--- src/main/java/com/what3words/code_challenge/CodeChallengeApplication.java
+++ src/main/java/com/what3words/code_challenge/CodeChallengeApplication.java
@@ -1,6 +1,7 @@
 package com.what3words.code_challenge;
 
 import com.what3words.javawrapper.What3WordsV3;
+import com.what3words.javawrapper.response.ConvertToCoordinates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +11,15 @@ public class CodeChallengeApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(CodeChallengeApplication.class, args);
+
 		// For all requests a what3words API key is needed
-		What3WordsV3 api = new What3WordsV3("what3words-api-key");
+		What3WordsV3 api = new What3WordsV3("RRLW7T7I");
+
+		String words = "silk.slap.soils";
+		WordsToCoordinates converter = new WordsToCoordinates();
+
+		ConvertToCoordinates coordinates = converter.getCoordinates(api, words);
+		//System.out.println(coordinates);
 	}
 
 }
---
+++ src/main/java/com/what3words/code_challenge/WordsToCoordinates.java
@@ -0,0 +1,37 @@
+package com.what3words.code_challenge;
+
+import com.what3words.javawrapper.What3WordsV3;
+import com.what3words.javawrapper.response.APIResponse;
+import com.what3words.javawrapper.response.ConvertToCoordinates;
+
+public class WordsToCoordinates {
+
+    public ConvertToCoordinates getCoordinates(What3WordsV3 api, String words) {
+
+        // Convert a 3 word address to coordinates
+        ConvertToCoordinates coordinates = api.convertToCoordinates(words).execute();
+
+
+        if (coordinates.isSuccessful()) { // the request was successful
+            System.out.println("Coordinates: " + coordinates);
+
+        } else { // the request was not successful
+            APIResponse.What3WordsError error = coordinates.getError();
+
+            if (error == APIResponse.What3WordsError.BAD_WORDS) { // The three word address provided is invalid
+                System.out.println("BadWords: " + error.getMessage());
+
+            } else if (error == APIResponse.What3WordsError.INTERNAL_SERVER_ERROR) { // Server Error
+                System.out.println("InternalServerError: " + error.getMessage());
+
+            } else if (error == APIResponse.What3WordsError.NETWORK_ERROR) { // Network Error
+                System.out.println("NetworkError: " + error.getMessage());
+
+            } else { // Unknown Error
+                System.out.println(error + ": " + error.getMessage());
+
+            }
+        }
+        return coordinates;
+    }
+}
