6e738200c6755e7bd3a5ea21a41935b5cead9803
Elena Martina <ele_na98@live.it>
2019-06-11 16:04:44 +0100

    added w3w package and instantiated w3w class

--- src/main/java/com/what3words/code_challenge/CodeChallengeApplication.java
+++ src/main/java/com/what3words/code_challenge/CodeChallengeApplication.java
@@ -1,5 +1,6 @@
 package com.what3words.code_challenge;
 
+import com.what3words.javawrapper.What3WordsV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodeChallengeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CodeChallengeApplication.class, args);
+		// For all requests a what3words API key is needed
+		What3WordsV3 api = new What3WordsV3("what3words-api-key");
 	}
 
 }
